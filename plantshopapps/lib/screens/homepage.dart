f06d154fc4f1a012d583534ba2970b6ee45dd9f9
ngocdu <ngocdu171@gmail.com>
2022-05-24 17:58:22 +0300

    create drawer

--- plantshopapps/lib/screens/homepage.dart
+++ plantshopapps/lib/screens/homepage.dart
@@ -1,15 +1,27 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  final style = const TextStyle(fontWeight: FontWeight.bold, color: Colors.black);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: AppBar(
         title: const Text('HomePage'),
       ),
-      body: Container(),
+      body: ListView(
+        children: <Widget>[
+          Text('Trang chu', style: GoogleFonts.roboto(textStyle: style),),
+          Text('Cua hang'),
+          Text('Gioi thieu'),
+          Text('Lien he'),
+          Text('Bai viet'),
+        ],
+      ),
     );
   }
 }
