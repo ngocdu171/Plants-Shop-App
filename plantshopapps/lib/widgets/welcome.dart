adf235d151f3b4658fc1c003ca055a5583491c3e
ngocdu <ngocdu171@gmail.com>
2022-05-31 16:21:51 +0300

    css welcome

--- plantshopapps/lib/widgets/welcome.dart
+++ plantshopapps/lib/widgets/welcome.dart
@@ -9,31 +9,40 @@ class Welcome extends StatelessWidget {
       child: Column(
         children: [
           Image.asset("assets/images/Ficus Lyrata-Fiddle leaf Fig Plant.jpg"),
-          const Text("Plant a tree so that next generation can get air for free"),
+          const Text("Plant a tree so that next generation can get air for free",
+            style: TextStyle(
+              fontSize: 20,
+              color: Color.fromARGB(255, 0, 0, 0)
+            ),
+          ),
           SizedBox(
-            width: 80,
+            width: 90,
             child: TextButton(
                 style: TextButton.styleFrom(
                   backgroundColor: Colors.green.shade900,
                 ),
                 onPressed: () {},
-                child: Row(
-                  children: [
-                    const Text(
-                      'Start',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Color.fromARGB(255, 255, 255, 255),
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 5),
+                        child: Text(
+                          'Start',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                          ),
+                        ),
                       ),
-                    ),
-                    Image.asset(
-                      "assets/icons/icons8-arrow-64.png",
-                      width: 20,
-                      color: Colors.white,
-                    )
-                  ],
-                )),
+                      Image.asset(
+                        "assets/icons/icons8-arrow-64.png",
+                        width: 20,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
           ),
         ],
       ),
