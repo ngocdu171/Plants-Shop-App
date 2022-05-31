430d90ef6485af99d2974cef1d4406de5d40037f
ngocdu <ngocdu171@gmail.com>
2022-05-31 17:07:05 +0300

    css welcome widget

--- plantshopapps/lib/widgets/welcome.dart
+++ plantshopapps/lib/widgets/welcome.dart
@@ -9,17 +9,40 @@ class Welcome extends StatelessWidget {
       child: Column(
         children: [
           Image.asset("assets/images/Ficus Lyrata-Fiddle leaf Fig Plant.jpg"),
-          const Text("Plant a tree so that next generation can get air for free",
-            style: TextStyle(
-              fontSize: 20,
-              color: Color.fromARGB(255, 0, 0, 0)
+          RichText(
+            text: const TextSpan(
+              style: TextStyle(
+                fontSize: 30,
+                color: Color.fromARGB(255, 9, 47, 16)
+              ),
+              children: [
+                TextSpan(text: 'PLANT ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+                TextSpan(text: 'a ', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 161, 145, 0))),
+                TextSpan(text: 'TREE \n', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+                TextSpan(text: 'and \n', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 161, 145, 0) )),
+                TextSpan(text: 'Get ', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 161, 145, 0) )),
+                TextSpan(text: 'OXYGEN \n', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+                TextSpan(text: 'For ', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 161, 145, 0) )),
+                TextSpan(text: 'FREE', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+              ]
+              // children: [
+              //   TextSpan(text: 'PLANT ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+              //   TextSpan(text: 'a '),
+              //   TextSpan(text: 'TREE \n', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+              //   TextSpan(text: 'so that \n'),
+              //   TextSpan(text: 'NEXT GENERATION ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+              //   TextSpan(text: 'Get '),
+              //   TextSpan(text: 'AIR \n', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+              //   TextSpan(text: 'For '),
+              //   TextSpan(text: 'FREE', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45)),
+              // ]
             ),
           ),
           SizedBox(
-            width: 90,
+            width: 100,
             child: TextButton(
                 style: TextButton.styleFrom(
-                  backgroundColor: Colors.green.shade900,
+                  backgroundColor: const Color.fromARGB(255, 9, 47, 16),
                 ),
                 onPressed: () {},
                   child: Row(
@@ -29,7 +52,8 @@ class Welcome extends StatelessWidget {
                         child: Text(
                           'Start',
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
+                            fontWeight: FontWeight.w700,
+                            letterSpacing: 2,
                             fontSize: 20,
                             color: Color.fromARGB(255, 255, 255, 255),
                           ),
