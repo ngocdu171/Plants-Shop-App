84b599f1f48a62968aa1fc204a724196bbc6eaae
ngocdu <ngocdu171@gmail.com>
2022-06-09 00:58:26 +0300

    create list hot plants

--- plantshopapps/lib/widgets/recommend.dart
+++ plantshopapps/lib/widgets/recommend.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 List<Hotplants> hotPlants = [
-  Hotplants(url: 'assets/images/Aloe Vera.jpg', name: 'Aloe Vera', price: 10),
-  Hotplants(url: 'assets/images/Fern Plant.jpg', name: 'Fern Plant', price: 20),
-  Hotplants(url: 'assets/images/Ficus Lyrata-Fiddle leaf Fig Plant.jpg', name: 'Ficus Lyrata', price: 30),
-  Hotplants(url: 'assets/images/Jade-Plant.jpg', name: 'Jade-Plant', price: 10),
-  Hotplants(url: 'assets/images/palm.jpg', name: 'palm', price: 20),
-  Hotplants(url: 'assets/images/snapdragon_Outdoor.jpg', name: 'snapdragon_Outdoor', price: 30),
+  Hotplants(url: "assets/images/Aloe Vera.jpg", name: "Aloe Vera", price: "10"),
+  Hotplants(url: "assets/images/Fern Plant.jpg", name: "Fern Plant", price: "20"),
+  Hotplants(url: "assets/images/Ficus Lyrata-Fiddle leaf Fig Plant.jpg", name: "Ficus Lyrata", price: "30"),
+  Hotplants(url: "assets/images/Jade-Plant.jpg", name: "Jade-Plant", price: "10"),
+  Hotplants(url: "assets/images/palm.jpg", name: "palm", price: "20"),
+  Hotplants(url: "assets/images/snapdragon_Outdoor.jpg", name: "snapdragon_Outdoor", price: "30"),
 ];
 
 class Hotplants {
   String url;
   String name;
-  int price;
+  String price;
 
   Hotplants({required this.url, required this.name, required this.price});
 }
@@ -23,19 +23,52 @@ class Recommend extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final plants = hotPlants.map((hotplant) {
-      final name = hotplant.name.toString();
+    // final plants = hotPlants.map((hotplant) {
+    //   final name = hotplant.name.toString();
+    //   final url = hotplant.url.toString();
 
-      return Text(name,
-      style: const TextStyle(fontSize: 20, color: Colors.red),);
-    }).toList();
+    //   return
+    //     Text(name,
+    //     style: const TextStyle(fontSize: 20, color: Colors.red),);
+    //     Text(url);
+    // }).toList();
+    final urlplant = hotPlants.map((e) => e.url).toString();
+    final nameplant = hotPlants.map((e) => e.name).toString();
+    final priceplpant = hotPlants.map((e) => e.price).toString();
 
-    return Column(
-      children: [
-        const Text("asjdhgasdgasd"),
-        const SizedBox(height: 4,),
-        ...plants
-      ],
+    return Container(
+      padding: const EdgeInsets.all(10),
+      height: 300,
+      child: GridView.builder(
+        itemCount: hotPlants.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount:3,
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            width: 200,
+            height: 500,
+            child: Column(
+              children: [
+                Image.asset(hotPlants[index].url, width: 90, height: 70,),
+                Text(hotPlants[index].name),
+                Text(hotPlants[index].price)
+              ],
+            ),
+          );
+        },
+        
+          // children: [
+          //   // Image.asset(url)
+          //   // const Text("asjdhgasdgasd"),
+          //   // SizedBox(height: 4,),
+          //   Image.asset(urlplant, width: 5,),
+          //   Text(nameplant),
+          //   Text(priceplpant)
+          // ],
+      ),
     );
 
     // return Container(
