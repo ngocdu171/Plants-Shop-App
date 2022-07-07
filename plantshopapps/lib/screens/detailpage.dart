e720168fb1a051be33ff8fda0458d61eba24ec29
ngocdu <ngocdu171@gmail.com>
2022-07-07 16:23:11 +0300

    show information of plant

--- plantshopapps/lib/screens/detailpage.dart
+++ plantshopapps/lib/screens/detailpage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:plantshopapps/data/plants.dart';
+// import 'package:plantshopapps/data/plants.dart';
+import 'package:plantshopapps/widgets/app_bar.dart';
+import 'package:plantshopapps/widgets/footer.dart';
 
 class DetailPage extends StatelessWidget {
   // final PlantsList plantsList;
@@ -16,9 +18,34 @@ class DetailPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Text(title),
-            Image.asset(url),
-            Text(price)
+            CustomAppBar(),
+            Container(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).padding.top,
+                left: 25,
+                right: 25
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  Center(child: Image.asset(url, width: 200, height: 200,)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(title),
+                      const Text("Store: 99"),
+                      
+                    ],
+                  ),
+                  const Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eros eros, faucibus vitae felis eu, sodales pharetra leo. Vivamus tempor, dui ut rhoncus vehicula, justo tellus sollicitudin urna, nec posuere orci tellus a magna. Integer nec laoreet nibh, vitae egestas augue. In semper ut massa sed dignissim. Praesent nec maximus.",
+                    maxLines: 1, overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(price),
+                ],
+              ),
+            ),
+            Footer()
           ],
         ),
       ),
