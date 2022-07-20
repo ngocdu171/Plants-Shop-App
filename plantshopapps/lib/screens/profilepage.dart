65da3105a7ba5495b31c495757767aa2499810ba
ngocdu <ngocdu171@gmail.com>
2022-07-20 13:18:53 +0300

    function logout

--- plantshopapps/lib/screens/profilepage.dart
+++ plantshopapps/lib/screens/profilepage.dart
@@ -8,8 +8,7 @@ class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  // State<ProfilePage> createState() => _ProfilePageState();
-  _ProfilePageState createState() => _ProfilePageState();
+  State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
