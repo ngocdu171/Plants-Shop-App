482d772845703f58473e23a33852ae13b2bf149e
ngocdu <ngocdu171@gmail.com>
2022-07-20 08:31:02 +0300

    Create homeTab and profileTab

--- plantshopapps/lib/main.dart
+++ plantshopapps/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:plantshopapps/screens/homepage.dart';
 import 'package:plantshopapps/screens/loginpage.dart';
 
 Future<void> main() async {
@@ -28,7 +29,8 @@ class MyApp extends StatelessWidget {
         // ignore: deprecated_member_use
         accentColor: const Color(0xFFFFEBC7),
       ),
-      home: const LoginPage(),
+      home: const HomePage(),
+      // home: const LoginPage(),
     );
   }
 }
--- plantshopapps/lib/screens/homepage.dart
+++ plantshopapps/lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plantshopapps/screens/profilepage.dart';
 import 'package:plantshopapps/widgets/aboutus.dart';
 import 'package:plantshopapps/widgets/app_bar.dart';
 import 'package:plantshopapps/widgets/category.dart';
@@ -7,42 +8,97 @@ import 'package:plantshopapps/widgets/recommend.dart';
 import 'package:plantshopapps/widgets/welcome.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  List <Widget> _widgetOptions = <Widget>[
+    const Text('Home'),
+    const ProfilePage()
+  ];
+
+  void _onItemTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomAppBar(),
-            Welcome(),
-            Category(),
-            Recommend(),
-            AboutUs(),
-            Footer()
-          ],
-        ),
-      )
-      // drawer: const Drawer(),
-      // appBar: AppBar(
-      //   title: const Text('HomePage'),
-      //   // backgroundColor: const Color.fromARGB(255, 20, 141, 18),
-      // ),
-      // body: const Body()
-      // // ListView(
-      // //   children: const <Widget>[
-      // //     Welcome(),
-      // //     Text('Category', style: TextStyle(fontSize: 40),),
-      // //     Category(),
-      // //     Text('Recommend', style: TextStyle(fontSize: 40),),
-      // //     Recommend(),
-      // //     AboutUs(),
-      // //     Footer(),
-      // //   ],
-      // // ),
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile'
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTap,
+      ),
     );
   }
 }
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             CustomAppBar(),
+//             Welcome(),
+//             Category(),
+//             Recommend(),
+//             AboutUs(),
+//             Footer()
+//           ],
+//         ),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Home'
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.person),
+//             label: 'Profile'
+//           ),
+//         ],
+//       ),
+//       // drawer: const Drawer(),
+//       // appBar: AppBar(
+//       //   title: const Text('HomePage'),
+//       //   // backgroundColor: const Color.fromARGB(255, 20, 141, 18),
+//       // ),
+//       // body: const Body()
+//       // // ListView(
+//       // //   children: const <Widget>[
+//       // //     Welcome(),
+//       // //     Text('Category', style: TextStyle(fontSize: 40),),
+//       // //     Category(),
+//       // //     Text('Recommend', style: TextStyle(fontSize: 40),),
+//       // //     Recommend(),
+//       // //     AboutUs(),
+//       // //     Footer(),
+//       // //   ],
+//       // // ),
+//     );
+//   }
+// }
