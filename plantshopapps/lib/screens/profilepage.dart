cf88e2bbe5c224ddac974c193003d504622903eb
ngocdu <ngocdu171@gmail.com>
2022-07-20 08:33:54 +0300

    Profile widget

---
+++ plantshopapps/lib/screens/profilepage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top,
+        left: 25,
+        right: 25
+      ),
+      child: Column(
+        children: <Widget>[
+              SizedBox(
+                height: 150,
+                child: Image.asset("assets/images/logo.png", fit: BoxFit.contain),
+              ),
+              const Text(
+                "Welcome Back",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              
+              const SizedBox(
+                height: 15,
+              ),
+            ],
+      ),
+    );
+  }
+}
