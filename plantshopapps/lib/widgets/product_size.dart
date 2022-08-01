32944e2d109474061a1d42aa8cdd9de3144378f9
ngocdu <ngocdu171@gmail.com>
2022-08-01 11:53:01 +0300

    create product size widget

--- plantshopapps/lib/widgets/product_size.dart
+++ plantshopapps/lib/widgets/product_size.dart
@@ -9,29 +9,40 @@ class ProductSize extends StatefulWidget {
 }
 
 class _ProductSizeState extends State<ProductSize> {
+
+  int _selected = 0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 20),
       child: Row(
         children: [
-          for(var i=0; i<widget.sizeList.length; i++)
-            Container(
-              width: 42,
-              height: 42,
-              decoration: BoxDecoration(
-                color: const Color(0xFFDCDCDC),
-                borderRadius: BorderRadius.circular(8)
-              ),
-              alignment: Alignment.center,
-              margin: const EdgeInsets.symmetric(
-                horizontal: 4
-              ),
-              child: Text(widget.sizeList[i],
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 16
-              ),
+          for(var i = 0; i < widget.sizeList.length; i++)
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _selected = i;
+                });
+              },
+              child: Container(
+                width: 42,
+                height: 42,
+                decoration: BoxDecoration(
+                  color: _selected == i ? const Color.fromARGB(255, 255, 127, 127) : const Color.fromARGB(255, 203, 203, 203),
+                  borderRadius: BorderRadius.circular(8)
+                ),
+                alignment: Alignment.center,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 4
+                ),
+                child: Text(widget.sizeList[i],
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: _selected == i ? Colors.white : Colors.black,
+                    fontSize: 16
+                  ),
+                ),
               ),
             )
         ],
