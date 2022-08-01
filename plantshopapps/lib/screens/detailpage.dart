2456c6d395827ebbb810fbeee1def47f18a1c78a
ngocdu <ngocdu171@gmail.com>
2022-08-01 12:31:42 +0300

    function addToCart

--- plantshopapps/lib/screens/detailpage.dart
+++ plantshopapps/lib/screens/detailpage.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:plantshopapps/widgets/image_swipe.dart';
 import 'package:plantshopapps/widgets/product_size.dart';
@@ -13,18 +14,30 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   final CollectionReference _productsRef =
-    FirebaseFirestore.instance.collection("products");
+      FirebaseFirestore.instance.collection("products");
+
+  final CollectionReference _usersRef =
+      FirebaseFirestore.instance.collection("users");
+
+  final User? _user = FirebaseAuth.instance.currentUser;
+
+  Future addToCart() {
+    return _usersRef
+        .doc(_user?.uid)
+        .collection("Cart")
+        .doc(widget.productId)
+        .set({"size": 1});
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Stack(
         children: [
           FutureBuilder<dynamic>(
             future: _productsRef.doc(widget.productId).get(),
             builder: (context, snapshot) {
-              if(snapshot.hasError) {
+              if (snapshot.hasError) {
                 return Scaffold(
                   body: Center(
                     child: Text("Error: ${snapshot.error}"),
@@ -32,7 +45,7 @@ class _DetailPageState extends State<DetailPage> {
                 );
               }
 
-              if(snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.connectionState == ConnectionState.done) {
                 Map<String, dynamic> product = snapshot.data?.data();
                 List imageList = product['image'];
                 List sizeList = product['size'];
@@ -43,44 +56,40 @@ class _DetailPageState extends State<DetailPage> {
                     ImageSwipe(imageList: imageList),
                     Padding(
                       padding: const EdgeInsets.only(
-                        top: 24,
-                        left: 24,
-                        right: 24,
-                        bottom: 4
-                      ),
-                      child: Text(product['name'].toString(), style: const TextStyle(fontWeight: FontWeight.bold,
-                        fontSize: 24),
+                          top: 24, left: 24, right: 24, bottom: 4),
+                      child: Text(
+                        product['name'].toString(),
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24),
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
-                        vertical: 4,
-                        horizontal: 24
+                          vertical: 4, horizontal: 24),
+                      child: Text(
+                        product['price'].toString() + '€',
+                        style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Color.fromARGB(255, 255, 39, 39)),
                       ),
-                      child: Text(product['price'].toString() + '€', style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        color: Color.fromARGB(255, 255, 39, 39)
-                      ),),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(
-                        vertical: 8,
-                        horizontal: 24
+                          vertical: 8, horizontal: 24),
+                      child: Text(
+                        product['description'],
+                        style: const TextStyle(fontSize: 16),
                       ),
-                      child: Text(product['description'], style: const TextStyle(
-                        fontSize: 16
-                      ),),
                     ),
                     const Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 24,
-                        horizontal: 24
+                      padding:
+                          EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+                      child: Text(
+                        "Select Size:",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
                       ),
-                      child: Text("Select Size:", style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16
-                      ),),
                     ),
                     ProductSize(sizeList: sizeList),
                     Padding(
@@ -91,28 +100,34 @@ class _DetailPageState extends State<DetailPage> {
                             width: 65,
                             height: 65,
                             decoration: BoxDecoration(
-                              color: const Color.fromARGB(255, 203, 203, 203),
-                              borderRadius: BorderRadius.circular(12)
-                            ),
+                                color: const Color.fromARGB(255, 203, 203, 203),
+                                borderRadius: BorderRadius.circular(12)),
                             alignment: Alignment.center,
-                            child: Image.asset("assets/icons/icons8-like-64.png", height: 40,),
+                            child: Image.asset(
+                              "assets/icons/icons8-like-64.png",
+                              height: 40,
+                            ),
                           ),
                           Expanded(
-                            child: Container(
-                              height: 65,
-                              margin: const EdgeInsets.only(
-                                left: 16
+                            child: GestureDetector(
+                              onTap: () async {
+                                addToCart();
+                              },
+                              child: Container(
+                                height: 65,
+                                margin: const EdgeInsets.only(left: 16),
+                                decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(12)),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  "Add to Cart",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
                               ),
-                              decoration: BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(12)
-                              ),
-                              alignment: Alignment.center,
-                              child: const Text("Add to Cart", style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600
-                              ),),
                             ),
                           )
                         ],
