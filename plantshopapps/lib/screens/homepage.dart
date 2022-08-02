591248803dca2e610dd051f3ff8f4ea07e432cad
ngocdu <ngocdu171@gmail.com>
2022-08-02 13:35:44 +0300

    css home page

--- plantshopapps/lib/screens/homepage.dart
+++ plantshopapps/lib/screens/homepage.dart
@@ -1,15 +1,6 @@
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:plantshopapps/screens/detailpage.dart';
-// import 'package:plantshopapps/widgets/aboutus.dart';
-// import 'package:plantshopapps/widgets/category.dart';
-// import 'package:plantshopapps/widgets/footer.dart';
-// import 'package:plantshopapps/widgets/recommend.dart';
-// import 'package:plantshopapps/widgets/welcome.dart';
-
-import '../data/plants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,164 +12,105 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   //firestore
   final CollectionReference _productsRef =
-    FirebaseFirestore.instance.collection("products");
+      FirebaseFirestore.instance.collection("products");
 
   @override
   Widget build(BuildContext context) {
-    final test = plantsList.where((element) => element.discount == true);
-
-  return Scaffold(
+    return Scaffold(
       body: Container(
-          child: Stack(
-            children: [
-              FutureBuilder<QuerySnapshot>(
-                future: _productsRef.get(),
-                builder: (context, snapshot) {
-                  if(snapshot.hasError) {
-                    return Scaffold(
-                      body: Center(
-                        child: Text("Error: ${snapshot.error}"),
-                      ),
-                    );
-                  }
-
-                  //Collection data ready to show
-                  if(snapshot.connectionState == ConnectionState.done) {
-                    return ListView(
-                      children: snapshot.data!.docs.map((product) {
-                        return GestureDetector(
-                          onTap: () {
-                            // Navigator.push(context, MaterialPageRoute(
-                            //   builder: (context) => DetailPage(title: title, id: id, url: url, price: price)
-                            // ));
-                          },
-                          child: Container(
-                            child: Column(
-                              children: [
-                                Text(product['name']),
-                                Card(
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                                  child: Image.network(product['image'][0], width: 90, height: 90,)
-                                ),
-                                Text(product['name']),
-                                Text(product['price'].toString())
-                              ],
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    );
-                  }
-
-                  //Loading State
-                  return const Scaffold(
+        child: Stack(
+          children: [
+            FutureBuilder<QuerySnapshot>(
+              future: _productsRef.get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Scaffold(
                     body: Center(
-                      child: CircularProgressIndicator(),
+                      child: Text("Error: ${snapshot.error}"),
                     ),
                   );
-                },
-              )
-            ],
-          ),
+                }
+
+                //Collection data ready to show
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return ListView(
+                    children: snapshot.data!.docs.map((product) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      DetailPage(productId: product.id)));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: const Color.fromARGB(255, 236, 242, 239),
+                          ),
+                          height: 350,
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 12, horizontal: 24),
+                          child: Stack(
+                            children: [
+                              Container(
+                                  height: 300,
+                                  child: Center(
+                                    child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(12),
+                                        child: Image.network(
+                                          product['image'][0],
+                                          fit: BoxFit.cover,
+                                        )),
+                                  )),
+                              Positioned(
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 35, vertical: 10),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        product['name'] ?? "Product Name",
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      Text(
+                                        "${product['price']} €" ?? "Price",
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600,
+                                            color: Color.fromARGB(
+                                                255, 36, 186, 108)),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                }
+
+                //Loading State
+                return const Scaffold(
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
+      ),
     );
   }
-
-  //   return Scaffold(
-  //     body: SingleChildScrollView(
-  //       child: Container(
-  //         padding: EdgeInsets.only(
-  //           top: MediaQuery.of(context).padding.top,
-  //           left: 25,
-  //           right: 25
-  //         ),
-  //         child: Column(
-  //           crossAxisAlignment: CrossAxisAlignment.start,
-  //           children: [
-  //             // const Text("Shop", style: TextStyle(
-  //             //   fontSize: 25, fontWeight: FontWeight.bold
-  //             // ),),
-  //             Row(
-  //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //               children: [
-  //                 Row(
-  //                   children: [
-  //                     const Text("New Arrival", style: TextStyle(
-  //                       fontSize: 20,
-  //                       fontWeight: FontWeight.bold
-  //                     ),),
-  //                     Container(
-  //                       child: Image.asset("assets/icons/icons8-expand-arrow-64.png", width: 20,),
-  //                     )
-  //                   ],
-  //                 ),
-  //                 Row(
-  //                   children: [
-  //                     const Text("Price", style: TextStyle(
-  //                       fontSize: 20,
-  //                       fontWeight: FontWeight.bold
-  //                     ),),
-  //                     Container(
-  //                       child: Image.asset("assets/icons/icons8-expand-arrow-64.png", width: 20,),
-  //                     )
-  //                   ],
-  //                 ),
-  //               ]
-  //             ),
-
-  //             Wrap(
-  //               alignment: WrapAlignment.start,
-  //               spacing: 15,
-  //               runSpacing: 20,
-  //               children: plantsList.map((e) => GestureDetector(
-  //                 onTap: () {
-  //                   Navigator.of(context).push(
-  //                     MaterialPageRoute(
-  //                       builder: (context) => DetailPage(id: e.id,
-  //                         title: e.name, url: e.url, price: e.price)
-  //                     )
-  //                   );
-  //                 },
-  //                 child: Column(
-  //                   children: [
-  //                     Card(
-  //                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-  //                       child: Image.asset(e.url, width: 90, height: 90,)
-  //                     ),
-  //                     Text(e.name),
-  //                     Text(e.price + " €")
-  //                   ],
-  //                 ),
-  //               )).toList(),
-  //             ),
-              
-  //           ],
-  //         ),
-  //       )
-  //     ),
-  //   );
-
-  // }
 }
-
-// class HomePage extends StatelessWidget {
-//   const HomePage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SingleChildScrollView(
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Welcome(),
-//             Category(),
-//             Recommend(),
-//             AboutUs(),
-//             Footer()
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
